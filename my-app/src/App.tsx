2ec45ceea39f31e8af2032612d21c0ec444533c8
vaishali <vaishalityagiajmera29@gmail.com>
2025-06-01 17:06:23 +0530

    lint fix

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-
 import './App.css';
 
-const App =()=> {
+const App: React.FC =()=> {
   return (
     <div className="App">
-    
+    { "helo world" }
     </div>
   );
 }
